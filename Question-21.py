0c975847fbe396cef7e2cae56607f0c4ae92beec
CherryGautam <93386069+CherryGautam@users.noreply.github.com>
2024-06-17 22:54:58 +0530

    Create Question-21.py

---
+++ Question-21.py
@@ -0,0 +1,7 @@
+x=input("Enter list elements separated by spaces: ").split()
+y=input("Enter the element whose occurance is to be found: ")
+o=0
+for i in x:
+    if(i==y):
+        o+=1
+print(y, "occurs in the list", o, "times")
